bf2a26c634d1c1bf2f10d5fc7c3f7a08b2310977
Angel Zuniga <mazuniga@sudamericano.edu.ec>
2024-06-12 04:13:56 -0500

    .

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -17,7 +17,7 @@ describe('AppComponent', () => {
   it(`should have the 'piramide-imagenes' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('piramide-imagenes');
+    expect(app.title).toEqual('piramide-imagenes'); 
   });
 
   it('should render title', () => {
